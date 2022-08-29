a5bbb0d973d6bf1fd14858dac12c8402acc549cb
Lee Owen <lee.owen1452@gmail.com>
2022-08-29 16:59:32 -0400

    created an int and printed it.

---
+++ src/utils/Foo.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'dave1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		int seven = 7;
+		System.out.println(seven);
+	}
+
+}
